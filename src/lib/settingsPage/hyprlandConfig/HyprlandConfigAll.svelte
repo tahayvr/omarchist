ce0a4c1a6246b3aa47a1a0b14a13db503edb5546
Taha <paprikataha@gmail.com>
2025-10-04 07:57:13 -0700

    configs are grid ion xl screens

--- src/lib/settingsPage/hyprlandConfig/HyprlandConfigAll.svelte
+++ src/lib/settingsPage/hyprlandConfig/HyprlandConfigAll.svelte
@@ -3,5 +3,11 @@
 	import HyprlandGeneral from './HyprlandGeneral.svelte';
 </script>
 
-<HyprlandGeneral />
-<HyprlandDecoration />
+<div class="flex flex-col gap-4 xl:grid xl:grid-cols-2">
+	<div class="grid-span-full">
+		<HyprlandGeneral />
+	</div>
+	<div class="grid-span-full">
+		<HyprlandDecoration />
+	</div>
+</div>
