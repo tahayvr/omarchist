02f1a2626cea04f8a5fe33d265b4d3dea15be0ad
Taha <paprikataha@gmail.com>
2025-10-10 01:01:53 -0700

    add toggle group component for filtering settings

---
+++ src/lib/settingsPage/SettingsFilterToggle.svelte
@@ -0,0 +1,8 @@
+<script>
+	import * as ToggleGroup from '$lib/components/ui/toggle-group/index.js';
+</script>
+
+<ToggleGroup.Root variant="outline" type="single">
+	<ToggleGroup.Item value="all">All</ToggleGroup.Item>
+	<ToggleGroup.Item value="basic" class="p-4">Basic</ToggleGroup.Item>
+</ToggleGroup.Root>
