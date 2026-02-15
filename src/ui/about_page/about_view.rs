116012e8a3eebda3bd3407bd8c55faeeab90dfa5
Taha Nejad <paprikataha@gmail.com>
2026-02-14 20:40:16 -0800

    Update about page

--- src/ui/about_page/about_view.rs
+++ src/ui/about_page/about_view.rs
@@ -1,12 +1,13 @@
+use gpui::FontWeight;
 use gpui::*;
-use gpui_component::{Icon, IconName, button::*, h_flex, v_flex};
+use gpui_component::{ActiveTheme, Icon, IconName, button::*, h_flex, v_flex};
 
 pub struct AboutView;
 
 impl Render for AboutView {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         v_flex()
-            .gap_2()
+            .gap_0()
             .size_full()
             .items_center()
             .justify_center()
@@ -17,19 +18,34 @@ impl Render for AboutView {
                     .h(px(128.))
                     .object_fit(ObjectFit::Contain),
             )
-            .child("v0.1.0")
+            .child(
+                div()
+                    .mt_6()
+                    .text_3xl()
+                    .font_weight(FontWeight::BOLD)
+                    .text_color(cx.theme().foreground)
+                    .child("OMARCHIST"),
+            )
+            .child(
+                div()
+                    .text_color(cx.theme().muted_foreground)
+                    .child("v0.1.0"),
+            )
             .child(
                 h_flex()
+                    .mt_12()
                     .gap_4()
                     .child(
-                        Button::new("btn")
+                        Button::new("x-com")
                             .icon(Icon::new(Icon::empty()).path("icons/x.svg"))
+                            .outline()
                             .cursor_pointer()
                             .on_click(|_, _, cx| cx.open_url("https://x.com/tahayvr/")),
                     )
                     .child(
                         Button::new("github")
                             .icon(IconName::GitHub)
+                            .outline()
                             .cursor_pointer()
                             .on_click(|_, _, cx| {
                                 cx.open_url("https://github.com/tahayvr/omarchist-rs")
